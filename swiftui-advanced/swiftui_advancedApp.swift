4c13530ed37f6717b8630dc0f2ed06991f27da90
tom lee <tom@akaai.kr>
2023-03-15 22:47:40 +0900

    first commit

---
+++ swiftui-advanced/swiftui_advancedApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_advancedApp.swift
+//  swiftui-advanced
+//
+//  Created by Tom Lee on 2023/03/15.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_advancedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
